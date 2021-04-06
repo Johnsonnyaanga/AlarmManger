e9cf7e5f4e003fddc0ffc44d0928b52f4870ad27
unknown <johnsonyaanga@gmail.com>
2021-04-06 16:32:04 +0300

    update

--- app/src/main/java/com/example/alarmmanager/MainActivity.kt
+++ app/src/main/java/com/example/alarmmanager/MainActivity.kt
@@ -8,6 +8,7 @@ import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TimePicker
@@ -83,9 +84,12 @@ class MainActivity : AppCompatActivity() {
    @RequiresApi(Build.VERSION_CODES.KITKAT)
    private var onTimeSetListener = TimePickerDialog.OnTimeSetListener { view, hour, minute ->
        val calendar = Calendar.getInstance()
+       calendar.set(Calendar.MONTH,4)
+       calendar.set(Calendar.DAY_OF_MONTH,7)
        calendar.set(Calendar.HOUR_OF_DAY, hour)
        calendar.set(Calendar.MINUTE, minute)
        calendar.set(Calendar.SECOND, 0)
+       Log.d("dddf",calendar.toString())
        startAlarm(calendar)
 
     }
